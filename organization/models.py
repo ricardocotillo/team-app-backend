b2e72413a19d6d8191050e33d1d11be81fa9d2b2
ricardocotillo <ricardo.cotillo@gmail.com>
2022-02-18 21:20:21 -0500

    added functions

--- organization/models.py
+++ organization/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 from api.mixins import AutoCreatedUpdatedMixin
 
+class Member(AutoCreatedUpdatedMixin):
+    user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
+    org = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='members')
+    nickname = models.CharField(max_length=16, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    admin = models.BooleanField(default=False)
+
+    def make_admin(self):
+        self.admin = True
+        self.save()
+
 # Create your models here.
 class Organization(AutoCreatedUpdatedMixin):
     name = models.CharField(max_length=200)
@@ -13,13 +24,5 @@ class Organization(AutoCreatedUpdatedMixin):
     def __str__(self):
         return self.name
 
-
-class Member(AutoCreatedUpdatedMixin):
-    user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
-    org = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='members')
-    nickname = models.CharField(max_length=16)
-    active = models.BooleanField(default=True)
-    admin = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.nickname
+    def add_member(self, user, **kwargs):
+        return self.members.get_or_create(user=user, **kwargs)
